889a116b38e70ea71e07c6cf29287a19153724e8
Thien Ho <thien.hophu@gmail.com>
2023-02-11 14:26:33 +0700

    Add Header comp and photo materials

---
+++ src/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+import logo from './logo.svg';
+
+const Header = () => (
+  <header className="header">
+    <img src={logo} className="logo" alt="logo" />
+  </header>
+);
+
+export default Header;
